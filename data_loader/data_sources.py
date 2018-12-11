d9d89ce5fd464460888b6b167ffacb25130b1117
Bauke Vendeloo <b.j.h.vendeloo@student.tudelft.nl>
2018-12-11 11:51:59 +0100

    Function month transformation

--- data_loader/data_sources.py
+++ data_loader/data_sources.py
@@ -2,15 +2,18 @@
 
 import pandas_datareader as web
 import pandas as pd
+import datetime
+from datetime import date
+from dateutil.relativedelta import relativedelta
 from pytrends.request import TrendReq
 pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
 pd.set_option('display.width', 1000)
 
 
-def get_google_trends_data(keyword):
+def get_google_trends_data(keyword, timeframe):
     """
-    Requires the pyrends library
+    Requires the pytrends library
     Please visit the GitHub link for detailed explanation of the "unofficial API for Google Trends" called pytrends
     GitHub link: https://github.com/GeneralMills/pytrends
     :param keyword: a list of str format elements of the keywords to be searched
@@ -18,12 +21,12 @@ def get_google_trends_data(keyword):
     :return: a pandas DataFrame object
     """
     pytrend_obj = TrendReq()
-    pytrend_obj.build_payload(keyword, cat=0, timeframe='today 3-m', geo='', gprop='')
+    pytrend_obj.build_payload(keyword, cat=0, timeframe = timeframe, geo='', gprop='')
     interest_over_time_df = pytrend_obj.interest_over_time()
     data = interest_over_time_df[keyword]
     return data
 
-def get_stocks_data(stock_idx = "AAPL", start_date = "01/01/2014", end_date = "12/06/2018"):
+def get_stocks_data(stock_idx = "AAPL", start_date = "01/01/2004", end_date = "12/06/2018"):
     """
     Requires the pandas-datareader library
     :param stock_idx: str of yahoo index be default is "AAPL"
@@ -44,6 +47,20 @@ def get_stocks_data(stock_idx = "AAPL", start_date = "01/01/2014", end_date = "1
     data.insert(0, "Dates", datetime_dates_list)
     # data.insert(0, "Dates", wk_days)
     return data
+
+def date_transfromer(begin_date='2004-01-01',end_date=date.today(),interval='3m'):
+    emptylist = []
+    year = int(begin_date[0:4])
+    month = int(begin_date[5:7])
+    day = int(begin_date[8:10])
+
+    datum = datetime.date(year,month,day)
+    while datum < end_date:
+        emptylist.append(datum)
+        datum = datum + relativedelta(days=+90)
+    return emptylist
+
+
 
 
 
