e6176991891939c57952a56d6d54ebfd16623176
mvanlobensels <31916708+TSLokken@users.noreply.github.com>
2018-12-11 12:28:21 +0100

    added EMA and MACD

--- technical_indicators/technical_indicators.py
+++ technical_indicators/technical_indicators.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 
 
-def simple_moving_avg(data, trend_period_days=21):
+def SMA(data, trend_period_days=21):
     """
     The Simple Moving Average (Rolling Window) Indicator
     :param data: The original OHCLV and VWAP pandas DataFrame output of i.e. the data_loader.data_sources.get_stocks_data()
@@ -18,6 +18,9 @@ def simple_moving_avg(data, trend_period_days=21):
 
 
 def RSI(dataframe, column="Close", period=14):
+    """
+    Relative Strength Index
+    """
 
     delta = dataframe[column].diff()
     up, down = delta.copy(), delta.copy()
@@ -31,3 +34,34 @@ def RSI(dataframe, column="Close", period=14):
     rsi = 100 - 100 / (1 + roll_up / roll_down)
 
     return dataframe.join(rsi.to_frame('RSI'))
+
+
+def EMA(data, trend_period_days=21, return_data = False):
+    """
+    Exponential Moving Average
+    """
+    trend_period_name = str(trend_period_days) + "d"
+    dt_dates_list = data.index.date.tolist()
+    close = data.Close
+    trend = data.Close.ewm(span=trend_period_days).mean()
+    ema_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, trend_period_name:trend.values}, index=range(len(dt_dates_list)))
+    
+    if return_data is False:     
+        return ema_trend
+    else:
+        return trend
+
+
+def MACD(data, ema_days_upper=26, ema_days_lower=12):
+    """
+    Moving Average Convergence Divergence
+    """
+    dt_dates_list = data.index.date.tolist()
+    close = data.Close
+    
+    ema_trend_26d = EMA(data, trend_period_days=ema_days_upper, return_data=True)
+    ema_trend_12d = EMA(data, trend_period_days=ema_days_lower, return_data=True)
+    trend = ema_trend_26d - ema_trend_12d
+    macd_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, "MACD":trend.values })
+    
+    return macd_trend
