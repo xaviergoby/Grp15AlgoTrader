3472a91e8b70be9e8fbe5472a6326b76b6e65e3f
timomuller <t.muller-1@student.tudelft.nl>
2018-12-11 11:07:30 +0100

    Rsi function added.

--- technical_indicators/simple_moving_avg.py
+++ technical_indicators/simple_moving_avg.py
@@ -16,3 +16,18 @@ def simple_moving_avg(data, trend_period_days=21):
     sma_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, trend_period_name:trend.values}, index=range(len(dt_dates_list)))
     return sma_trend
 
+
+def RSI(dataframe, column="Close", period=14):
+
+    delta = dataframe[column].diff()
+    up, down = delta.copy(), delta.copy()
+
+    up[up < 0] = 0
+    down[down > 0] = 0
+
+    roll_up = up.ewm(com=period - 1, adjust=False).mean()
+    roll_down = down.ewm(com=period - 1, adjust=False).mean().abs()
+
+    rsi = 100 - 100 / (1 + roll_up / roll_down)
+
+    return dataframe.join(rsi.to_frame('RSI'))
