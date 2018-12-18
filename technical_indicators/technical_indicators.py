1b560dfc116c27c2f98df651da21cd91ae088245
mvanlobensels <31916708+TSLokken@users.noreply.github.com>
2018-12-18 12:40:40 +0100

    MACD trend line, not fully functioning yet

--- technical_indicators/technical_indicators.py
+++ technical_indicators/technical_indicators.py
@@ -36,23 +36,25 @@ def RSI(dataframe, column="Close", period=14):
     return dataframe.join(rsi.to_frame('RSI'))
 
 
-def EMA(data, trend_period_days=21, return_data = False):
+def EMA(data, trend_period_days=21, return_data=False):
     """
     Exponential Moving Average
     """
     trend_period_name = str(trend_period_days) + "d"
     dt_dates_list = data.index.date.tolist()
-    close = data.Close
-    trend = data.Close.ewm(span=trend_period_days).mean()
-    ema_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, trend_period_name:trend.values}, index=range(len(dt_dates_list)))
     
-    if return_data is False:     
+    if return_data is False:
+        close = data.Close
+        trend = data.Close.ewm(span=trend_period_days).mean()
+        ema_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, trend_period_name:trend.values}, index=range(len(dt_dates_list)))
         return ema_trend
-    else:
+    
+    elif return_data is True:
+        trend = data.ewm(span=trend_period_days).mean()
         return trend
 
 
-def MACD(data, ema_days_upper=26, ema_days_lower=12):
+def MACD(data, ema_days_upper=26, ema_days_lower=12, return_data=False):
     """
     Moving Average Convergence Divergence
     """
@@ -62,6 +64,28 @@ def MACD(data, ema_days_upper=26, ema_days_lower=12):
     ema_trend_26d = EMA(data, trend_period_days=ema_days_upper, return_data=True)
     ema_trend_12d = EMA(data, trend_period_days=ema_days_lower, return_data=True)
     trend = ema_trend_26d - ema_trend_12d
-    macd_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, "MACD":trend.values })
+    macd_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.values, "MACD":trend.values }, index=range(len(dt_dates_list)))
     
-    return macd_trend
+    if return_data is False:
+        return macd_trend
+    elif return_data is True:
+        return trend
+
+
+def MACD_signal_line(data):
+    """
+    Moving Average Convergence Divergence signal line. The MACD signal line is the difference between the MACD and the 9-day EMA of the MACD series,
+    where the MACD is the difference between the 12-day an 26-day EMA. A positive signal line indicates a buy signal. A negative signal line a sell signal.  
+    """
+    dt_dates_list = data.index.date.tolist()
+    close = data.Close
+    
+    macd = MACD(data, return_data=True)
+    ema_9d = EMA(macd, trend_period_days=9, return_data=True)
+    signal_line = macd - ema_9d
+    signal_line_trend = pd.DataFrame({"Dates":dt_dates_list, "Close":close.valeus, "MACD":macd.values, "9d EMA":ema_9d.values, "Signal Line":signal_line}, index=range(len(dt_dates_list)))
+    
+    return signal_line_trend
+    
+
+
